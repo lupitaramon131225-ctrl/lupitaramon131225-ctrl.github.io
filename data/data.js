750dd6f5b174322032af2f14d3b7a0968fd15f1e
lupitaramon131225-ctrl <lupitaramon131225@gmail.com>
2026-05-14 19:17:03 -0600

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -150,6 +150,11 @@ const categoryData = {
       "9.jpeg",
       "10.jpeg",
       "11.jpeg",
+      "12.jpeg",
+      "13.jpeg",
+      "14.jpeg",
+      "15.jpeg",
+      "16.jpeg",
     ]
   },
   "propuestas_matrimonio": {
@@ -410,6 +415,18 @@ const categoryData = {
       "21.jpeg",
       "22.jpeg",
       "23.jpeg",
+      "24.jpeg",
+      "25.jpeg",
+      "26.jpeg",
+      "27.jpeg",
+      "28.jpeg",
+      "29.jpeg",
+      "30.jpeg",
+      "31.jpeg",
+      "32.jpeg",
+      "33.jpeg",
+      "34.jpeg",
+      "35.jpeg",
     ]
   },
   "xv": {
