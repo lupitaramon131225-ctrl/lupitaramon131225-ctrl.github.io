ac4df80aaa22e548e198586234b239208f153d5f
lupitaramon131225-ctrl <lupitaramon131225@gmail.com>
2026-05-26 17:18:17 -0600

    Add files via upload

--- data/data.js
+++ data/data.js
@@ -1,4 +1,4 @@
-﻿// ESTE ARCHIVO SE GENERA AUTOMÁTICAMENTE. No edites directamente.
+// ESTE ARCHIVO SE GENERA AUTOMÁTICAMENTE. No edites directamente.
 // Ejecuta: node scripts/generate-gallery-data.js
 
 const categoryData = {
@@ -155,6 +155,24 @@ const categoryData = {
       "14.jpeg",
       "15.jpeg",
       "16.jpeg",
+      "17.jpeg",
+      "18.jpeg",
+      "19.jpeg",
+      "20.jpeg",
+      "21.jpeg",
+      "22.jpeg",
+      "23.jpeg",
+      "24.jpeg",
+      "25.jpeg",
+      "26.jpeg",
+      "27.jpeg",
+      "28.jpeg",
+      "29.jpeg",
+      "30.jpeg",
+      "31.jpeg",
+      "32.jpeg",
+      "33.jpeg",
+      "34.jpeg",
     ]
   },
   "propuestas_matrimonio": {
